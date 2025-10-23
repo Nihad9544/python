7403707971b7d49bd3acc3b10e2951e9753bdb8a
Nihad9544 <nihadckdr@gmail.com>
2025-10-23 10:57:24 +0530

    completed

---
+++ experiment14.1/circl_module.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import math
+def area(radius):
+    return(math.pi*radius**2)
+def perimeter(radius):
+    return 2*(math.pi*radius)
+
---
+++ experiment14.1/rect_module.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#. Create modules Rectangle, Circle. Include methods to find area and perimeter of respective figures in each module. Write programs that finds area and perimeter of figures by different importing statements.
+def area(length,width):
+    return(length*width)
+def perimeter(length,width):
+    return 2*(length+width)
+    
+
